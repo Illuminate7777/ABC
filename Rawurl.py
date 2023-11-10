bd9fa21df22f24740478404d660d1e092155b388
Illuminate7777 <149811871+Illuminate7777@users.noreply.github.com>
2023-11-09 20:07:24 -0600

    Delete Rawurl.py

--- Rawurl.py
+++
@@ -1,7 +0,0 @@
-# your link goes here
-
-link = "https://github.com/Illuminate7777/ABC/blob/main/STUDENT%20RESPONSIBILITY.txt"
-
-# note: this will break if a repo/organization or subfolder is named "blob" -- would be ideal to use a fancy regex
-# to be more precise here
-print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))
